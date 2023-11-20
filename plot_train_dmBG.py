28ecfb58bf8ba1e6edc646a3409daad91edbb5d0
syer <erik.syniawa@informatik.tu-chemnitz.de>
2023-11-20 12:44:29 +0100

    Modified the weight plot due to enhancement of tactile points and initial posiitons.

--- plot_train_dmBG.py
+++ plot_train_dmBG.py
@@ -85,8 +85,10 @@ def plot_w_training_dm(simID, goal, cons, save_name=None):
 
 def plot_w_matrix_training(simID, goal, save_name=None):
     import matplotlib.pyplot as plt
-    from parameters import create_results_folder
+    from parameters import create_results_folder, model_params
 
+    dimGP = list(model_params['dim_medial_BG'])
+    dimPFC = model_params['num_goals']
     weights = load_monitors(simID, goal, 'w')
 
     direct_pathway_cons = ['PFCdStrD1', 'StrD1GPi']
@@ -100,62 +102,75 @@ def plot_w_matrix_training(simID, goal, save_name=None):
         keys_indirect.extend([key for key in weights.keys() if con in key])
 
     # plotting
-    timestamps = [0, weights[keys_direct[0]].shape[0]//2, weights[keys_direct[0]].shape[0]]
+    timestamps = [4, 8, 12]
     nt = len(timestamps)
 
-    scal = 2.0
-    fig = plt.figure(num=5, figsize=(20, 12))
+    # figure attributes
+    minimum = 0.0
+    maximum = 0.5
+    fig = plt.figure(num=5, figsize=(15, 15))
+
+    # norms
+    norm_w_direct = np.matmul(np.transpose(weights[keys_direct[0]][0, :, :]),
+                              np.transpose(weights[keys_direct[1]][0, :, :]))
+
+    norm_w_indirect = np.matmul(np.transpose(weights[keys_indirect[0]][0, :, :]),
+                                np.transpose(weights[keys_indirect[1]][0, :, :]))
+
+    # reshape to (1, #body positions, #tactile points)
     for i, timestamp in enumerate(timestamps):
-        print(weights[keys_direct[0]].shape)
-        print(weights[keys_direct[1]].shape)
 
-        w1 = np.transpose(weights[keys_direct[0]][timestamp - 1, :, :])
-        w2 = np.transpose(weights[keys_direct[1]][timestamp - 1, :, :])
-        w_direct = np.matmul(w1, w2)
+        w_direct = np.matmul(np.transpose(weights[keys_direct[0]][timestamp - 1, :, :]),
+                             np.transpose(weights[keys_direct[1]][timestamp - 1, :, :])) - norm_w_direct
 
-        w1 = np.transpose(weights[keys_indirect[0]][timestamp - 1, :, :])
-        w2 = np.transpose(weights[keys_indirect[1]][timestamp - 1, :, :])
-        w_indirect = np.matmul(w1, w2)
+        w_indirect = np.matmul(np.transpose(weights[keys_indirect[0]][timestamp - 1, :, :]),
+                               np.transpose(weights[keys_indirect[1]][timestamp - 1, :, :])) - norm_w_indirect
 
-        plt.figtext(0.05, 0.75 - 0.2725 * i, f"Trial = {timestamp}", fontsize=18, ha='center')
+        # reshape to (1, #body positions, #tactile points)
+        w_direct = w_direct.reshape([dimPFC] + dimGP)
+        w_indirect = w_indirect.reshape([dimPFC] + dimGP)
+
+        plt.figtext(0.07, 0.75 - 0.2725 * i, f"Trial = {timestamp}", fontsize=24, ha='center')
 
         # direct pathway
         plt.subplot(nt, 2, i * 2 + 1)
-        pcm = plt.imshow(w_direct, vmin=0, vmax=scal, cmap='Purples')
-        plt.ylabel('dPFC', fontsize=16)
-        plt.xlabel('GPi', fontsize=16)
+        pcm = plt.imshow(w_direct[goal, :, :], vmin=minimum, vmax=maximum, cmap='Purples')
+        plt.ylabel('dPFC', fontsize=20)
+        plt.xlabel('GPi', fontsize=20)
         plt.xticks([])
         plt.yticks([])
         if i == 0:
-            plt.title('Direct pathway:', fontsize=20)
+            plt.title('Direct pathway', fontsize=24)
         if i == nt - 1:
             ax = fig.gca()
             cax = ax.inset_axes([0.1, -0.3, 0.8, 0.1], transform=ax.transAxes)
-            cbar = fig.colorbar(pcm, ax=ax, cax=cax, ticks=[0, scal], orientation='horizontal')
-            cbar.ax.tick_params(labelsize=24)
+            cbar = fig.colorbar(pcm, ax=ax, cax=cax, ticks=[minimum, maximum], orientation='horizontal')
+            cbar.ax.tick_params(labelsize=18)
+            cbar.set_label('$ \delta w $', labelpad=-15, fontsize=20, weight='bold')
 
-        # lines
-        plt.plot([2.5, 2.5], [-0.5, 2.5], color='black', linewidth=1)
-        plt.plot([5.5, 5.5], [-0.5, 2.5], color='black', linewidth=1)
+        # plot lines to diffentiate the different body positions
+        for j in range(dimGP[0]):
+            plt.plot([- 0.5, -0.5 + dimGP[1]], [0.5 + j, 0.5 + j], color='gray', linewidth=1)
 
         # indirect pathway
         plt.subplot(nt, 2, i * 2 + 2)
-        pcm = plt.imshow(w_indirect, vmin=0, vmax=scal, cmap='Purples')
-        plt.ylabel('dPFC', fontsize=16)
-        plt.xlabel('GPe', fontsize=16)
+        pcm = plt.imshow(w_indirect[goal, :, :], vmin=minimum, vmax=maximum, cmap='Purples')
+        plt.ylabel('dPFC', fontsize=20)
+        plt.xlabel('GPe', fontsize=20)
         plt.xticks([])
         plt.yticks([])
         if i == 0:
-            plt.title('Indirect pathway:', fontsize=20)
+            plt.title('Indirect pathway', fontsize=24)
         if i == nt - 1:
             ax = fig.gca()
             cax = ax.inset_axes([0.1, -0.3, 0.8, 0.1], transform=ax.transAxes)
-            cbar = fig.colorbar(pcm, ax=ax, cax=cax, ticks=[0, scal], orientation='horizontal')
-            cbar.ax.tick_params(labelsize=24)
+            cbar = fig.colorbar(pcm, ax=ax, cax=cax, ticks=[minimum, maximum], orientation='horizontal')
+            cbar.ax.tick_params(labelsize=18)
+            cbar.set_label('$ \delta w $', labelpad=-15, fontsize=20, weight='bold')
 
-        # lines
-        plt.plot([2.5, 2.5], [-0.5, 2.5], color='black', linewidth=1)
-        plt.plot([5.5, 5.5], [-0.5, 2.5], color='black', linewidth=1)
+        # plot lines to diffentiate the different body positions
+        for j in range(dimGP[0]):
+            plt.plot([- 0.5, -0.5 + dimGP[1]], [0.5 + j, 0.5 + j], color='gray', linewidth=1)
 
     # save
     if save_name is not None:
@@ -173,5 +188,4 @@ if __name__ == '__main__':
     # plot_rates_training_dm(0, 0, ['PFC', 'StrD1', 'GPi', 'VA', 'SNc'], save_name='direct_Test_trial_complete_1.png')
     # plot_rates_training_dm(0, 0, ['PFC', 'StrD2', 'GPe', 'StrThal', 'VA', 'SNc'], save_name='indirect_Test_trial_complete_1.png')
     # plot_w_training_dm(0, 0, ['PFC', 'GPe', 'GPi'], save_name='Test_trial_wDA+_2.png')
-    plot_w_matrix_training(0, 0)
-    
+    plot_w_matrix_training(0, 0, 'BG_learning.pdf')
